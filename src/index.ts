cdce13bae6427cf43d37fcd65982be30fc86deda
Alhassan Faisal <fastpmp@Alhassans-MacBook-Air.local>
2020-02-19 19:43:09 +0000

    Add confirm alert to sweet alert functions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-import Swal, { SweetAlertResult, SweetAlertIcon } from "sweetalert2";
+import Swal, { SweetAlertResult, SweetAlertIcon } from 'sweetalert2';
 
 export default class SweetalertService {
-  showErrorMessage(title: string, message: string, redirect = "") {
-    return this.showAlert(title, message, redirect, "error");
+  showErrorMessage(title: string, message: string, redirect = '') {
+    return this.showAlert(title, message, redirect, 'error');
   }
 
-  showInfoMessage(title: string, message: string, redirect = "") {
-    return this.showAlert(title, message, redirect, "info");
+  showInfoMessage(title: string, message: string, redirect = '') {
+    return this.showAlert(title, message, redirect, 'info');
   }
 
-  showSuccessMessage(title: string, message: string, redirect = "") {
-    return this.showAlert(title, message, redirect, "success");
+  showSuccessMessage(title: string, message: string, redirect = '') {
+    return this.showAlert(title, message, redirect, 'success');
+  }
+
+  showConfirmMessage(
+    title: string = 'Confirm',
+    message: string,
+    critical: boolean = false,
+    criticalMessage: string = 'Do you want to continue?'
+  ): any {
+    return Swal.fire({
+      title,
+      html: `
+      <div class="text-center">
+        <div>${title}</div>
+        <div>${message}</div>
+        ${critical ? '<div><b>' + criticalMessage + '</b></div>' : ''}
+      </div>`,
+      icon: 'question',
+      showCancelButton: false
+    }).then(confirm => {
+      if (confirm) {
+        return 'continue';
+      }
+    });
   }
 
   showPleaseWait(message: string) {
@@ -25,7 +48,7 @@ export default class SweetalertService {
   showAlert(
     title: string,
     message: string,
-    redirect: string = "",
+    redirect: string = '',
     type: SweetAlertIcon
   ) {
     return Swal.fire({
