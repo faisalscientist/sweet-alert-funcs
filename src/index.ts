87810b3c931aff30a2d18d628dec18bef7a2678c
Alhassan Faisal <fastpmp@Alhassans-MacBook-Air.local>
2020-02-19 19:43:25 +0000

    1.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import Swal, { SweetAlertResult, SweetAlertIcon } from 'sweetalert2';
+import Swal, { SweetAlertResult, SweetAlertIcon } from "sweetalert2";
 
 export default class SweetalertService {
-  showErrorMessage(title: string, message: string, redirect = '') {
-    return this.showAlert(title, message, redirect, 'error');
+  showErrorMessage(title: string, message: string, redirect = "") {
+    return this.showAlert(title, message, redirect, "error");
   }
 
-  showInfoMessage(title: string, message: string, redirect = '') {
-    return this.showAlert(title, message, redirect, 'info');
+  showInfoMessage(title: string, message: string, redirect = "") {
+    return this.showAlert(title, message, redirect, "info");
   }
 
-  showSuccessMessage(title: string, message: string, redirect = '') {
-    return this.showAlert(title, message, redirect, 'success');
+  showSuccessMessage(title: string, message: string, redirect = "") {
+    return this.showAlert(title, message, redirect, "success");
   }
 
   showConfirmMessage(
-    title: string = 'Confirm',
+    title: string = "Confirm",
     message: string,
     critical: boolean = false,
-    criticalMessage: string = 'Do you want to continue?'
+    criticalMessage: string = "Do you want to continue?"
   ): any {
     return Swal.fire({
       title,
@@ -25,13 +25,13 @@ export default class SweetalertService {
       <div class="text-center">
         <div>${title}</div>
         <div>${message}</div>
-        ${critical ? '<div><b>' + criticalMessage + '</b></div>' : ''}
+        ${critical ? "<div><b>" + criticalMessage + "</b></div>" : ""}
       </div>`,
-      icon: 'question',
+      icon: "question",
       showCancelButton: false
     }).then(confirm => {
       if (confirm) {
-        return 'continue';
+        return "continue";
       }
     });
   }
@@ -48,7 +48,7 @@ export default class SweetalertService {
   showAlert(
     title: string,
     message: string,
-    redirect: string = '',
+    redirect: string = "",
     type: SweetAlertIcon
   ) {
     return Swal.fire({
